52a48d5abb6f103223c8a54b27b7f5c7d97622f5
jorgechato <hack@jorgechato.com>
2023-10-07 20:39:08 +0900

    UPDATE area with BONES function and response structure

--- getArea.go
+++ getArea.go
@@ -2,13 +2,47 @@ package main
 
 import "github.com/gin-gonic/gin"
 
-func getArea(c *gin.Context) {
-	long := c.Query("long")
-	lat := c.Query("lat")
+type Condition string
 
-	c.JSON(200, gin.H{
-		"long":    long,
-		"lat":     lat,
-		"message": "getArea",
+const (
+	ConditionGood Condition = "good"
+	ConditionBad  Condition = "bad"
+)
+
+type AquaticLocation struct {
+	Name        string    `json:"name"`
+	Condition   Condition `json:"condition"`
+	Temperature float64   `json:"temperature"`
+	Distance    float64   `json:"distance"`
+}
+
+type AreaResponse struct {
+	Location               Location        `json:"location"`
+	NearestAquaticLocation AquaticLocation `json:"nearest_aquatic_location"`
+}
+
+func getArea(c *gin.Context) {
+	location := c.Request.Context().Value("location").(Location)
+
+	c.JSON(200, AreaResponse{
+		Location: location,
+		NearestAquaticLocation: AquaticLocation{
+			Name:        "Baltic Sea",
+			Condition:   ConditionGood,
+			Temperature: 17.8,
+			Distance:    14.97,
+		},
 	})
+}
+
+func getClosestSea(location Location) (string, float64) {
+	return "", 0
+}
+
+func getAvgCondition(location Location) float64 {
+	return 0
+}
+
+func getAvgTemperature(location Location) float64 {
+	return 0
 }
