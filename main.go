9306d52e826ab0f360cbdb1e7da7494e74453bd3
jorgechato <hack@jorgechato.com>
2023-10-07 14:46:46 +0900

    ADD basic endoints

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/api/v1/graph/:long/:lat", getGraph)
+	r.GET("/api/v1/area/:long/:lat", getArea)
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
